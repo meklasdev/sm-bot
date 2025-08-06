814f8a94c73ca2e47b2b967962a926884c37280a
meklasdev <meklasdev@gmail.com>
2025-08-06 19:03:41 +0200

    Add verification config

---
+++ src/config/verificationConfig.js
@@ -0,0 +1,22 @@
+module.exports = {
+    VERIFY_CHANNEL_ID: '1402678764923523267',
+    VERIFY_ROLE_ID: '1402678555531284681',
+    TIMEOUT_MS: 60 * 1000,
+    DM_MESSAGE: `⛔ Twoja weryfikacja została odrzucona.
+
+Aby uzyskać dostęp do darmowych rzeczy, musisz **wysłać DWA zdjęcia** potwierdzające wykonanie poniższych kroków:
+
+✅ Obserwacja TikToka: https://www.tiktok.com/@slientmafiatop  
+✅ Subskrypcja + like pod filmem: https://www.youtube.com/watch?v=DOBELRa6apA
+
+Spróbuj ponownie za 1 minutę. Dziękujemy! – Silent Maf1a 🏴‍☠️`,
+    STICKY_MESSAGE: `### <a:yes:1253433659260669975> Verification - maximum 2 minutes.
+
+-# To access our **Free Stuff**, please make sure you have completed **ALL** steps and attached **TWO clear and complete screenshots** as proof:
+
+## <a:Verified_Purple_Animated:1382655410795843695> **Follow us on TikTok:** [www.tiktok.com/@slientmafiatop](https://www.tiktok.com/@slientmafiatop)
+
+## <a:Verified_Purple_Animated:1382655410795843695> **Leave a like & sub on the channel:** [youtube.com/watch?v=DOBELRa6apA](https://www.youtube.com/watch?v=DOBELRa6apA)
+
+-# Thank you for your support - Silent Maf1a <:silent:1395058293432516658>`
+};
